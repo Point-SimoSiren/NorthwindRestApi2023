8f9d43e68bbb7fdb6b12c3ba946cab6ce817f399
Simo Sirén <simo.siren1@careeria.fi>
2023-11-01 10:39:57 +0200

    Lisäystoiminto

--- NorthwindRestApi/Controllers/CustomersController.cs
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NorthwindRestApi.Models;
+using System.Linq.Expressions;
 
 namespace NorthwindRestApi.Controllers
 {
@@ -15,10 +16,64 @@ namespace NorthwindRestApi.Controllers
         [HttpGet]
         public ActionResult GetAllCustomers()
         {
-            var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+            try
+            {
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+            }
+            catch(Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
         }
 
         
+
+        // Hakee yhden asiakkaan pääavaimella
+        [HttpGet("{id}")]
+        public ActionResult GetOneCustomerById(string id)
+        {
+            try
+            {
+                var asiakas = db.Customers.Find(id);
+                if (asiakas != null)
+                {
+                    return Ok(asiakas);
+                }
+                else
+                {
+                    //return BadRequest("Asiakasta id:llä " + id + " ei löydy."); // perinteinen tapa liittää muuttuja
+                    return NotFound($"Asiakasta id:llä {id} ei löydy."); // string interpolation -tapa
+                }
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e);
+            }
+           
+        }
+
+        // Uuden lisääminen
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Customer cust)
+        {
+            try
+            {
+                db.Customers.Add(cust);
+                db.SaveChanges();
+                return Ok($"Lisättiin uusi asiakas {cust.CompanyName} from {cust.City}");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
+        }
+
+
+
+
+
+
     }
 }
